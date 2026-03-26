b524099013d4f8ee245930d3430b45c05b6870fc
Rabbi01521 <rabbihasan113@gmail.com>
2026-03-26 19:26:10 +0600

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,118 @@
+import { FaEnvelope, FaFacebookSquare, FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
 import footerImg from "../../assets/logo.png";
 
 const Footer = () => {
   return (
-    <div className="bg-[#001931] text-base-300">
-      <footer className="footer sm:footer-horizontal p-10 max-w-[1200px] mx-auto ">
-        <aside>
-          <div>
-            <img className="w-16" src={footerImg} alt="" />
+    <div className="bg-black">
+      <div className="max-w-[1200px] mx-auto text-gray-200 border-b-2 border-gray-300 py-8 px-5">
+        <div className="lg:grid lg:grid-cols-2">
+          <div className="">
+            <h1 className="mb-3 text-xl lg:text-xl flex items-center">
+              <img className="w-12 mr-3" src={footerImg} alt="" /> Hero---IO
+            </h1>
+            <p className="text-xs md:pr-0 lg:pr-50">
+              A modern app marketplace designed to help users explore, install, and manage applications effortlessly. Built with a focus on performance, simplicity, and a seamless user experience across all devices.
+            </p>
           </div>
-          <p>
-            Hero IO
-            <br />
-            Providing reliable tech since 2025
-          </p>
-        </aside>
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </nav>
-      </footer>
+
+          <div className="lg:flex md:grid md:grid-cols-2 md:gap-5">
+            <div className="lg:px-5 mt-5 lg:mt-0">
+              <h2 className="mb-3 text-xl font-bold lg:text-xl">Company</h2>
+              <ul>
+                <li>
+                  <a className="text-xs" href="#">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a className="text-xs " href="#">
+                    Our Mission
+                  </a>
+                </li>
+                <li>
+                  <a className="text-xs " href="#">
+                    Contact Saled
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="lg:px-0 mt-5 lg:mt-0">
+              <h2 className="mb-3 text-xl font-bold lg:text-xl">Services</h2>
+              <ul>
+                <li>
+                  <a className="text-xs " href="#">
+                    Products & Services
+                  </a>
+                </li>
+                <li>
+                  <a className="text-xs " href="#">
+                    Customer Stories
+                  </a>
+                </li>
+                <li>
+                  <a className="text-xs " href="#">
+                    Download Apps
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="lg:px-0 mt-5 lg:mt-0">
+              <h2 className="mb-3 text-xl font-bold lg:text-xl">Information</h2>
+              <ul>
+                <li>
+                  <a className="text-xs" href="#">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a className="text-xs" href="#">
+                    Terms & Conditions
+                  </a>
+                </li>
+                <li>
+                  <a className="text-xs" href="#">
+                    Join Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="lg:px-0 mt-5 lg:mt-0">
+              <h2 className="mb-3 text-xl font-bold lg:text-xl">
+                Social Links
+              </h2>
+              <ul className="flex justify-around items-center">
+                <li>
+                  <a className="text-lg items-center" href="#">
+                    <FaTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg" href="#">
+                    <FaLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg" href="#">
+                    <FaFacebookSquare />
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg" href="#">
+                    <FaEnvelope />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center py-5">
+        <span className="text-gray-400 text-xs font-light lg:text-sm md:text-sm">
+          © 2025 Hero-IO. All rights reserved.
+        </span>
+      </div>
     </div>
   );
 };
